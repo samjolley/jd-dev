67db4545e56fd8a552f2817151e9542b2317c662
samjolley <sam@jolleydigital.com>
2017-02-21 11:08:47 -0500

    various quiz form updates. button color change to orange

--- front-page.php
+++ front-page.php
@@ -176,7 +176,7 @@ function utility_pro_add_hard_website() {
 function utility_pro_add_can_do_1() {
 
 	printf( '<div %s>', genesis_attr( 'can-do' ) );
-	genesis_structural_wrap( 'can-do' );
+	genesis_structural_wrap( 'can-do');
 
 	genesis_widget_area(
 		'utility-can-do-1',
