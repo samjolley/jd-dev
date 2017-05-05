f395078f11ace85cbceff1a95758a4f6e05649db
samjolley <sam@jolleydigital.com>
2017-05-05 09:49:32 -0400

    restore to default front-page.php, functions.php, widget-areas.php

--- front-page.php
+++ front-page.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Front page for the JD Pro theme
+ * Front page for the Utility Pro theme
  *
- * @package JD_Pro
- * @author  Sam Jolley
+ * @package Utility_Pro
+ * @author  Carrie Dils
  * @license GPL-2.0+
  */
 
@@ -18,14 +18,8 @@ function utility_pro_homepage_setup() {
 
 	$home_sidebars = array(
 		'home_welcome' 	   => is_active_sidebar( 'utility-home-welcome' ),
+		'home_gallery_1'   => is_active_sidebar( 'utility-home-gallery-1' ),
 		'call_to_action'   => is_active_sidebar( 'utility-call-to-action' ),
-		'logos'   => is_active_sidebar( 'utility-logos' ),
-		'hard_website'   => is_active_sidebar( 'utility-hard-website' ),
-		'home_services'   => is_active_sidebar( 'utility-home-services' ),
-		'home_why'   => is_active_sidebar( 'utility-home-why' ),
-		'home_works_1'   => is_active_sidebar( 'utility-home-works-1' ),
-		'home-testimonials'   => is_active_sidebar( 'utility-home-testimonials' ),
-		'footer-cta'   => is_active_sidebar( 'utility-footer-cta' ),
 	);
 
 	// Return early if no sidebars are active.
@@ -44,60 +38,27 @@ function utility_pro_homepage_setup() {
 			add_action( 'genesis_after_header', 'utility_pro_add_home_welcome' );
 		}
 
+		// Add home gallery area if "Home Gallery 1" widget area is active.
+		if ( $home_sidebars['home_gallery_1'] ) {
+			add_action( 'genesis_after_header', 'utility_pro_add_home_gallery' );
+		}
+
 		// Add call to action area if "Call to Action" widget area is active.
 		if ( $home_sidebars['call_to_action'] ) {
 			add_action( 'genesis_after_header', 'utility_pro_add_call_to_action' );
 		}
-
-		// Add home logos area if "Logos Section" widget area is active.
-		if ( $home_sidebars['logos'] ) {
-			add_action( 'genesis_after_header', 'utility_pro_add_logos' );
-		}
-
-		// Add hard website area if "Hard Website" widget area is active.
-		if ( $home_sidebars['hard_website'] ) {
-			add_action( 'genesis_after_header', 'utility_pro_add_hard_website' );
-		}
-
-		// Add Home Services widget area if "Home Services" widget area is active.
-		if ( $home_sidebars['home_services'] ) {
-			add_action( 'genesis_after_header', 'utility_pro_add_home_services' );
-		}
-
-		// Add why me website area if "Why Me" widget area is active.
-		if ( $home_sidebars['home_why'] ) {
-			add_action( 'genesis_after_header', 'utility_pro_add_home_why' );
-		}
-
-		// Add how it works widget area if "How it Works 1" widget area is active.
-		if ( $home_sidebars['home_works_1'] ) {
-			add_action( 'genesis_after_header', 'utility_pro_add_home_works_1' );
-		}
-
-		// Add home Testimonials area if "Home Testimonials" widget area is active.
-		if ( $home_sidebars['home-testimonials'] ) {
-			add_action( 'genesis_after_header', 'utility_pro_add_home_testimonials' );
-		}
-
-		// Add footer cta area if "Footer CTA" widget area is active.
-		if ( $home_sidebars['footer-cta'] ) {
-			add_action( 'genesis_after_header', 'utility_pro_add_footer_cta' );
-		}
 	}
-
-
 
 	// Full width layout.
 	// Uncomment the filter below if you'd like a full-width layout on the front page.
-	add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
+	// add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
 
 	// Filter site title markup to include an h1.
 	add_filter( 'genesis_site_title_wrap', 'utility_pro_return_h1' );
 
 	// Remove standard loop and replace with loop showing Posts, not Page content.
 	remove_action( 'genesis_loop', 'genesis_do_loop' );
-	// We've already removed the loop. We don't want to add this custom one in.
-	// add_action ( 'genesis_loop', 'utility_pro_front_loop' );
+	add_action( 'genesis_loop', 'utility_pro_front_loop' );
 }
 
 /**
@@ -129,6 +90,51 @@ function utility_pro_add_home_welcome() {
 }
 
 /**
+ * Display content for the "Home Gallery" section.
+ *
+ * @since 1.0.0
+ */
+function utility_pro_add_home_gallery() {
+
+	printf( '<div %s>', genesis_attr( 'home-gallery' ) );
+	genesis_structural_wrap( 'home-gallery' );
+
+	genesis_widget_area(
+		'utility-home-gallery-1',
+		array(
+			'before' => '<div class="home-gallery-1 widget-area">',
+			'after'  => '</div>',
+		)
+	);
+
+	genesis_widget_area(
+		'utility-home-gallery-2',
+		array(
+			'before' => '<div class="home-gallery-2 widget-area">',
+			'after'  => '</div>',
+		)
+	);
+
+	genesis_widget_area(
+		'utility-home-gallery-3',
+		array(
+			'before' => '<div class="home-gallery-3 widget-area">',
+			'after'  => '</div>',
+		)
+	);
+	genesis_widget_area(
+		'utility-home-gallery-4',
+		array(
+			'before' => '<div class="home-gallery-4 widget-area">',
+			'after'  => '</div>',
+		)
+	);
+
+	genesis_structural_wrap( 'home-gallery', 'close' );
+	echo '</div>';
+}
+
+/**
  * Display content for the "Call to action" section.
  *
  * @since 1.0.0
@@ -143,144 +149,6 @@ function utility_pro_add_call_to_action() {
 		)
 	);
 }
-
-/**
- * Display content for the "Logos Section".
- *
- * @since 1.0.0
- */
-function utility_pro_add_logos() {
-
-	genesis_widget_area( 'utility-logos',
-		array(
-			'before' => '<div class="logos"><div class="wrap">',
-			'after' => '</div></div>',
-		)
-	);
-}
-
-/**
- * Display content for the "Hard to Build a Website Section".
- *
- * @since 1.0.0
- */
-function utility_pro_add_hard_website() {
-
-	genesis_widget_area( 'utility-hard-website',
-		array(
-			'before' => '<div class="hard-website"><div class="wrap">',
-			'after' => '</div></div>',
-		)
-	);
-}
-
-/**
- * Display content for the "Home Services" section.
- *
- * @since 1.0.0
- */
-function utility_pro_add_home_services() {
-
-	genesis_widget_area( 'utility-home-services',
-		array(
-			'before' => '<div class="home-services" id="services"><div class="wrap">',
-			'after' => '</div></div>',
-		)
-	);
-}
-
-/**
- * Display content for the "Why Me" section.
- *
- * @since 1.0.0
- */
-function utility_pro_add_home_why() {
-
-	genesis_widget_area( 'utility-home-why',
-		array(
-			'before' => '<div class="home-why" id="why"><div class="wrap">',
-			'after' => '</div></div>',
-		)
-	);
-}
-
-/**
- * Display content for the "Home Works" section.
- *
- * @since 1.0.0
- */
-function utility_pro_add_home_works_1() {
-
-	printf( '<div %s>', genesis_attr( 'home-works' ) );
-	genesis_structural_wrap( 'home-works' );
-
-	genesis_widget_area(
-		'utility-home-works-1',
-		array(
-			'before' => '<div class="home-works-1 widget-area">',
-			'after'  => '</div>',
-		)
-	);
-
-	genesis_widget_area(
-		'utility-home-works-2',
-		array(
-			'before' => '<div class="home-works-2 widget-area">',
-			'after'  => '</div>',
-		)
-	);
-
-	genesis_widget_area(
-		'utility-home-works-3',
-		array(
-			'before' => '<div class="home-works-3 widget-area">',
-			'after'  => '</div>',
-		)
-	);
-	genesis_widget_area(
-		'utility-home-works-4',
-		array(
-			'before' => '<div class="home-works-4 widget-area">',
-			'after'  => '</div>',
-		)
-	);
-
-	genesis_structural_wrap( 'home-works', 'close' );
-	echo '</div>';
-}
-
-/**
- * Display content for "Testimonials".
- *
- * @since 1.0.0
- */
-
-function utility_pro_add_home_testimonials() {
-
-	genesis_widget_area( 'utility-home-testimonials',
-		array(
-			'before' => '<div class="home-testimonials" id="home-testimonials"><div class="wrap">',
-			'after' => '</div></div>',
-		)
-	);
-}
-
-/**
- * Display content for "Footer CTA".
- *
- * @since 1.0.0
- */
-
-function utility_pro_add_footer_cta() {
-
-	genesis_widget_area( 'utility-footer-cta',
-		array(
-			'before' => '<div class="footer-cta" id="footer-cta"><div class="wrap">',
-			'after' => '</div></div>',
-		)
-	);
-}
-
 
 /**
  * Display latest posts instead of static page.
--- includes/widget-areas.php
+++ includes/widget-areas.php
@@ -2,9 +2,9 @@
 /**
  * Utility Pro.
  *
- * @package      JD_Pro
+ * @package      Utility_Pro
  * @link         http://www.carriedils.com/utility-pro
- * @author       Sam Jolley
+ * @author       Carrie Dils
  * @copyright    Copyright (c) 2015, Carrie Dils
  * @license      GPL-2.0+
  */
@@ -28,59 +28,29 @@ function utility_pro_register_widget_areas() {
 			'description' => __( 'This is the welcome section at the top of the home page.', 'utility-pro' ),
 		),
 		array(
-			'id'          => 'utility-hard-website',
-			'name'        => __( 'Hard Website', 'utility-pro' ),
-			'description' => __( 'This is the hard-to-build-a-website section on the home page.', 'utility-pro' ),
+			'id'          => 'utility-home-gallery-1',
+			'name'        => sprintf( _x( 'Home Gallery %d', 'Group of Home Gallery widget areas', 'utility-pro' ), 1 ),
+			'description' => sprintf( _x( 'Home Gallery %d widget area on home page.', 'Description of widget area', 'utility-pro' ), 1 ),
+		),
+		array(
+			'id'          => 'utility-home-gallery-2',
+			'name'        => sprintf( _x( 'Home Gallery %d', 'Group of Home Gallery widget areas', 'utility-pro' ), 2 ),
+			'description' => sprintf( _x( 'Home Gallery %d widget area on home page.', 'Description of widget area', 'utility-pro' ), 2 ),
+		),
+		array(
+			'id'          => 'utility-home-gallery-3',
+			'name'        => sprintf( _x( 'Home Gallery %d', 'Group of Home Gallery widget areas', 'utility-pro' ), 3 ),
+			'description' => sprintf( _x( 'Home Gallery %d widget area on home page.', 'Description of widget area', 'utility-pro' ), 3 ),
+		),
+		array(
+			'id'          => 'utility-home-gallery-4',
+			'name'        => sprintf( _x( 'Home Gallery %d', 'Group of Home Gallery widget areas', 'utility-pro' ), 4 ),
+			'description' => sprintf( _x( 'Home Gallery %d widget area on home page.', 'Description of widget area', 'utility-pro' ), 4 ),
 		),
 		array(
 			'id'          => 'utility-call-to-action',
 			'name'        => __( 'Call to Action', 'utility-pro' ),
 			'description' => __( 'This is the Call to Action section on the home page.', 'utility-pro' ),
-		),
-		array(
-			'id'          => 'utility-logos',
-			'name'        => __( 'Logos Section', 'utility-pro' ),
-			'description' => __( 'This is the Logos section on the home page.', 'utility-pro' ),
-		),
-		array(
-			'id'          => 'utility-home-services',
-			'name'        => __( 'Services', 'utility-pro' ),
-			'description' => __( 'This is the Home Services area on the home page.', 'utility-pro' ),
-		),
-		array(
-			'id'          => 'utility-home-why',
-			'name'        => __( 'Why Me', 'utility-pro' ),
-			'description' => __( 'This is the Why Me section on the home page.', 'utility-pro' ),
-		),
-		array(
-			'id'          => 'utility-home-works-1',
-			'name'        => sprintf( _x( 'How it Works %d', 'Group of How it Works widget areas', 'utility-pro' ), 1 ),
-			'description' => sprintf( _x( 'How it Works %d widget area on home page.', 'Description of widget area', 'utility-pro' ), 1 ),
-		),
-		array(
-			'id'          => 'utility-home-works-2',
-			'name'        => sprintf( _x( 'How it Works %d', 'Group of How it Works widget areas', 'utility-pro' ), 2 ),
-			'description' => sprintf( _x( 'How it Works %d widget area on home page.', 'Description of widget area', 'utility-pro' ), 2 ),
-		),
-		array(
-			'id'          => 'utility-home-works-3',
-			'name'        => sprintf( _x( 'How it Works %d', 'Group of How it Works widget areas', 'utility-pro' ), 3 ),
-			'description' => sprintf( _x( 'How it Works %d widget area on home page.', 'Description of widget area', 'utility-pro' ), 3 ),
-		),
-		array(
-			'id'          => 'utility-home-works-4',
-			'name'        => sprintf( _x( 'How it Works %d', 'Group of How it Works widget areas', 'utility-pro' ), 4 ),
-			'description' => sprintf( _x( 'How it Works %d widget area on home page.', 'Description of widget area', 'utility-pro' ), 4 ),
-		),
-		array(
-			'id'          => 'utility-home-testimonials',
-			'name'        => __( 'Testimonials', 'utility-pro' ),
-			'description' => __( 'This is the Home Testimonials area on the home page.', 'utility-pro' ),
-		),
-		array(
-			'id'          => 'utility-footer-cta',
-			'name'        => __( 'Footer CTA', 'utility-pro' ),
-			'description' => __( 'This is the Footer CTA area on the home page.', 'utility-pro' ),
 		),
 	);
 
